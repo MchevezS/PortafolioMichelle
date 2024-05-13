f4b33406a6d981db5a733f0ee53cebf38ab4f89b
MchevezS <saboriomichelle93@gmail.com>
2024-05-13 16:35:03 -0600

    portafolio imagenes y informacion ultimos retoques

--- src/js/funcion.js
+++ src/js/funcion.js
@@ -3,72 +3,69 @@ let contFormulario = document.getElementById("contFormulario")               // 
 let inputNombre = document.getElementById("inputNombre")                               // 2. tuve que crear esta variable
 let btnEntrar = document.getElementById("btnEntrar")                                  // 2. cree esta variable para poder hacer una validacion. 
 let btnLimpiar = document.getElementById("btnLimpiar")
-let registrarsenombre = document.getElementById("registrarsenombre")                 // 2.1                               
+let registrarsenombre = document.getElementById("registrarsenombre")                                        
 let inputApellidos = document.getElementById("inputApellidos")
 let inputEdad = document.getElementById("inputEdad")
 let inputTelefono = document.getElementById("inputTelefono")
 let inputCorreo = document.getElementById("inputCorreo")
 let inputContrasenia = document.getElementById("inputContrasenia")
 let btnRegistrarse = document.getElementById("btnRegistrarse") 
-let listaUsuario = []                                                    //...cree una variable llamada listaUsuario, esto es para solo los usiarios que se registran
 
 function mostrarFormulario(){
-contFormulario.classList.remove("hidden")                              //1. Accedemos al contenedor que tiene todos los inputs para manipular la clase, usamos el atributo classlist para realizarlo.
-contFormulario.classList.add("formulario")                            //1. Accedemos al contenedor que tiene todos los inputs para manipular la clase, usamos el atributo classlist para realizarlo.
+  contFormulario.classList.remove("hidden")                              //1. Accedemos al contenedor que tiene todos los inputs para manipular la clase, usamos el atributo classlist para realizarlo.
+  contFormulario.classList.add("formulario")                            //1. Accedemos al contenedor que tiene todos los inputs para manipular la clase, usamos el atributo classlist para realizarlo.
 }
-    try{
-        btnMostrarFormulario.addEventListener("click",()=>{                                 //1. aqui llame al boton inicio y le agregue addeventlistener, esto para cuando toque inicio aparezca el formulario
-        mostrarFormulario();
-    })
-     }catch(e){
-         console.log(e)
-     }
-    let inputNombreLocalStorage                                                     // 2. luego afuera de esta funcion cree esta variable pero le tuve que añadir el "localstorage" para que las cosas se guardaran aqui
-    let registrarsenombreLocalStorage                                                              //2.1
-    let inputApellidosLocalStorage
-    let inputEdadLocalStorage
-    let inputTelefonoLocalStorage
-    let inputCorreoLocalStorage
-    let inputContraseniaLocalStorage
-    function guardarDatos () {                                                                                  //2.1
-        listaUsuario.push(registrarsenombre.value)                                                                 
-        localStorage.setItem("Apellidos",inputApellidos.value)
-        localStorage.setItem("Edad",inputEdad.value)
-        localStorage.setItem("Telefono",inputTelefono.value)
-        localStorage.setItem("correoElectronico",inputCorreo.value)
-        localStorage.setItem("contrasenia",inputContrasenia.value)
-        localStorage.setItem("Nombre",listaUsuario)                                   //... 
-        let guardados = localStorage.getItem("Nombre")                             //..
-        
-        let nombrelista = guardados.split(",")                                   //...
-        console.log(nombrelista)                                                //...
+try{
+  btnMostrarFormulario.addEventListener("click",()=>{                                 //1. aqui llame al boton inicio y le agregue addeventlistener, esto para cuando toque inicio aparezca el formulario
+    mostrarFormulario();
+  })
+}catch(e){
+  console.log(e)
+}
+  let listaUsuario = JSON.parse(localStorage.getItem("usuarios")) || []                                                    //...cree una variable llamada listaUsuario, y listaUsuario lo que va a contener es la lista de las personas que se van a registrar.
+    
+    function guardarDatos () {                                                                                  // el guardar datos solo es el nombre de la funcion.
+      let datos = {                                                                                       // La variable datos guarda objectos o todo lo que esta  escrito adentro de ahi 
+        nombre: registrarsenombre.value,
+        apellidos: inputApellidos.value,
+        edad:inputEdad.value,
+        telefono:inputTelefono.value,
+        correo:inputCorreo.value,
+        contrasena:inputContrasenia.value
+      } 
+      
+      listaUsuario.push(datos)                                                          // aqui lo que hice fue el nombre de la variable y el push es para que se guarde todo lo que voy a escribir y escribo entre parentesis lo que me va a guardar 
+
+      localStorage.setItem("usuarios",JSON.stringify(listaUsuario))                                 // esta linea quiere decir que local me va a guardar todos los  usarios que estan dentro de la listaUsuarios y el json es para que se registren varias personas
     }
     try {
-        btnRegistrarse.addEventListener("click",()=>{
-            valida_enviar()
-            guardarDatos()
-        })
+      btnRegistrarse.addEventListener("click",()=>{
+          valida_enviar()
+          guardarDatos()
+      })
     } catch (error) {
+      
     }
+  
               
     // Aqui voy a comenzar a validar los datos.
     function valida_enviar() {
       if (registrarsenombre.value=="") {
-            alert("Escribe tu nombre")    
+            alert("AGREGA TUS DATOS")    
        }
 
     let Edadpersona = inputEdad.value
     let Edadconvertida = parseInt(Edadpersona)
     console.log(Edadpersona)
       if ( Edadconvertida >= 18) {
-        alert("tienes 18 o mas años.")
+        alert("TE HAS REGISTRADO CON ÉXITO ")
         
       } else {
-          if (Edadconvertida<18) {
+          if (Edadconvertida<18) { 
             alert("Tienes menos de 18 años.")
              }
         }  
-    
+     
     }
 
     let correoLocalStorage=localStorage.getItem("correoElectronico");
@@ -102,16 +99,14 @@ try {
 
   let article = document.getElementById("sobremihidden1")                                      // este let lo llame asi porque asi se llama la variable. el getelement lo que hace es llamr al id
   let enlace1 = document.getElementById("enlace")                                           // este let nombre asi solo porque quise. getelement lo nombre asi porque asi se llama el id
-  function sobremi() {      
-    let experiencias = document.getElementById("informacionproyecto")                                          // lo nombre "sobremi" porque tenia que crearle un nombre y ese nombre solo lo tengo aqui.
-    article.classList.toggle("hidden")                                                   //experiencias.style.display="none"  // el hidden que esta aqui es el class que esta dentro de article
+  function sobremi() {                                                                        // lo nombre "sobremi" porque tenia que crearle un nombre y ese nombre solo lo tengo aqui.
+      /*let experiencias = document.getElementById("informacionproyecto")   */  
+    article.classList.toggle("hidden")    // el hidden que esta aqui es el class que esta dentro de article
   }
 
   enlace.addEventListener("click",()=>{                                                // este enlance es el id que esta <a id="enlace" href="#sobremiid">
-
-    sobremi()                                                                         // este sobremi es el nombre que le di a la  funcion 
+     sobremi();                                                                         // este sobremi es el nombre que le di a la  funcion 
   })
-
 
   let experiencias = document.getElementById("informacionproyecto")             //aqui le cree una variable llamada experiencias
   let EnlaceProyecto = document.getElementById("enlaceproyecto")                 // cree la variable EnlaceProyecto
